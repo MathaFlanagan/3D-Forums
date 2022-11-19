b217577a96d979f1dfbcaf65ed5ea8171bb7515b
MathaFlanagan <117550510+MathaFlanagan@users.noreply.github.com>
2022-11-18 19:43:39 -0500

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,9 @@
+const signUpBtn = document.querySelector('signup-btn');
+const signInBtn = document.querySelector('signin-btn');
+const formswrapper = document.querySelector('forms-wrapper')
+signUpBtn.addEventListener("click", (e)=>{e.preventDefault
+});
+
+signInBtn.addEventListener("click", (e)=>{e.preventDefault();
+    formswrapper.classList.remove("change");
+});
